06630cd12e1036eed9e49a9cc34157e227ab8238
Fenex <friedewald24@gmail.com>
2021-03-06 09:10:37 +0100

    First Changes on the Player Controller...
    I cant tell if this works coz missing play test.
    
    In theory player can drill, move, cant turn rn, and get heat up while drilling

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
+    private Vector3 mousePosition;
+    private Vector3 direction;
+    private Rigidbody rb;
+    private Rigidbody rigidbodyTrigger;
+
+    public float movespeed = 10f;
+
+    private bool isDrilling = false;
+
+    private float heat = 0;
     // Start is called before the first frame update
     void Start() {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        followMouse();
+        if (!isDrilling) {
+            heat++;
+        }
+    }
+
+    void followMouse() {
+        //follow mouse cursor?
+        mousePosition = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (mousePosition - transform.position).normalized;
+        rb.velocity = new Vector3(direction.x * movespeed, direction.y * movespeed, 0);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Entered Trigger/ore");
+        isDrilling = true;
+    }
+
+    private void OnTriggerStay(Collider other) {
+        Debug.Log("Drilling");
+        heat--;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("Trigger Left");
+        isDrilling = false;
     }
 }
