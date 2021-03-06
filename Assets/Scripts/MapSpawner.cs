c86f2571a98a72aeb293a012028a8a07f26088ea
Rogue4u <34610995+BennicTV@users.noreply.github.com>
2021-03-06 14:34:39 +0100

    Endless

---
+++ Assets/Scripts/MapSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSpawner : MonoBehaviour
+{
+
+    public GameObject backgroundMap;
+    public float ySpawn = -24.96f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frames
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "BackgroundMap") 
+        {
+            Debug.Log("Spawn new Map");
+           
+            Instantiate(backgroundMap, new Vector3(0, ySpawn, 0), Quaternion.identity);
+            ySpawn -= 24.96f;
+
+
+        }
+    }
+}   
